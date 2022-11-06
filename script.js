f53448771a905cdce7b8e35efef44b7a94ef6c38
maka-NOZADZE <nozadze.maka@gmail.com>
2022-11-06 17:31:31 +0100

    dates calculated

---
+++ script.js
@@ -0,0 +1,15 @@
+const newYears = "1 Jan 2023";
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minuts = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+  console.log(days, hours, minuts, seconds);
+}
+//initial call
+
+countDown();
+setInterval(countDown, 1000);
